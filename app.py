38d63d52bd38bde9df1f65919b03e95a53d9733d
glitch <saniltison@gmail.com>
2020-07-25 21:49:40 -0400

    created the search.htlm page to get the key word input from the client
    created base.html page as a template for inheritance for other webpages
    
    created route definitions for search page and a place holder route definition that will be used to query the database for the reference

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flask_restful import Resource, Api, reqparse
 # from flask_jwt import JWT, jwt_required
 
@@ -15,6 +15,20 @@ api = Api(app)
 @app.route("/")
 def home():
   return render_template("index.html")
+
+@app.route("/search", methods=["POST", "GET"])
+def search():
+  if request.method == "POST":
+      key_word = request.form["key_word"]
+      return redirect(url_for("search_for_ref", word=key_word))
+  else:
+      return render_template("search.html")
+
+@app.route("/<word>")
+def search_for_ref(word):
+  return "this came from the search-for-ref route function in the app it will return the rows from the database"
+
+
 
 
 
