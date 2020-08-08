5c17c075ab9bd35e11eab26b98aeabbd0e7f8b8c
glitch <saniltison@gmail.com>
2020-08-07 22:24:28 -0400

    the create_tables.py file was modified to include the correct names of the database and the table inside the database
    the database with the correct name was created with the python script

--- ref/create_tables.py
+++ ref/create_tables.py
@@ -1,20 +1,20 @@
 import sqlite3
 
-connection = sqlite3.connect('toolbox.db')
+connection = sqlite3.connect('net_box.db')
 cursor = connection.cursor()
 
 
 # create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 # cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS networking_toolbox (task text, command text)"
+create_table = "CREATE TABLE net_box (id INTERGER PRIMARY KEY, manufacture string, device string, task text, command text, notes text)"
 cursor.execute(create_table)
 
-entry = ('glitch', 'cisco')
-insert_query = "INSERT INTO networking_toolbox VALUES (?, ?)"
-cursor.execute(insert_query, entry)
+tool = (1, 'cisco', 'router', 'show interfaces', 'show ip int br', 'show ip int br')
+insert_query = "INSERT INTO net_box VALUES (?, ?, ?, ?, ?, ?)"
+cursor.execute(insert_query, tool)
 
-select_query = "SELECT * FROM networking_toolbox"
+select_query = "SELECT * FROM net_box"
 for row in cursor.execute(select_query):
     print(row)
 
